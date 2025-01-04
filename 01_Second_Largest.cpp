10c009b31b0f9184cdf5fb5e7784d16989c44d86
Alan Reji <143480799+alanreji2004@users.noreply.github.com>
2025-01-04 16:50:47 +0530

    Create 01_Second_Largest.cpp

---
+++ 01_Second_Largest.cpp
@@ -0,0 +1,17 @@
+    int getSecondLargest(vector<int> &arr) {
+        
+        int size = arr.size();
+        int i,largest =-1,secondLargest = -1;
+        
+        for(int i=0;i<size;i++){
+           
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > secondLargest){
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
