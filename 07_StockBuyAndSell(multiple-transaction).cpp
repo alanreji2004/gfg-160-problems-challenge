c1e5a1b6f6055d89f1b611c305a5594cd89187c5
Alan Reji <143480799+alanreji2004@users.noreply.github.com>
2025-01-04 17:12:45 +0530

    Create 07_StockBuyAndSell(multiple-transaction).cpp

---
+++ 07_StockBuyAndSell(multiple-transaction).cpp
@@ -0,0 +1,9 @@
+    int maximumProfit(vector<int> &prices) {
+        int totalProfit = 0;
+        for(int i = 1;i < prices.size();i++){
+            if(prices[i] > prices[i-1]){
+                totalProfit += prices[i] - prices[i-1];
+            }
+        }
+        return totalProfit;
+    }
