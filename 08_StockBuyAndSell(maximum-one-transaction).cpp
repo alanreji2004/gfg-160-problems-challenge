f904a1625bbaf77966edce0a1bef560825f951f8
Alan Reji <143480799+alanreji2004@users.noreply.github.com>
2025-01-04 17:32:38 +0530

    Create 08_StockBuyAndSell(maximum-one-transaction).cpp

---
+++ 08_StockBuyAndSell(maximum-one-transaction).cpp
@@ -0,0 +1,8 @@
+    int maximumProfit(vector<int> &prices) {
+        int tempMin=prices[0],profit =0;
+        for(int i = 0;i < prices.size();i++){
+           tempMin = min(tempMin,prices[i]);
+           profit = max(profit,prices[i] - tempMin);
+        }
+        return profit;
+    }
